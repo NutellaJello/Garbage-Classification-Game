f571e7ddf40f61bf4c85aadb9f518533289d207a
Alex Gan <92696056+NutellaJello@users.noreply.github.com>
2026-04-27 17:27:17 -0400

    Add multiple image entries with labels to script.js

--- script.js
+++ script.js
@@ -4,7 +4,26 @@ const images = [
   { src: "images/Image3.jpg", label: "Plastic" },
   { src: "images/Image4.jpg", label: "Plastic" },
   { src: "images/Image5.jpg", label: "Paper" },
-  { src: "images/Image6.jpg", label: "Plastic" }
+  { src: "images/Image6.jpg", label: "Plastic" }, 
+  { src: "images/000001%20(7).jpg", label: "Plastic" }, 
+  { src: "images/000002%20(2).jpg", label: "Cardboard" }, 
+  { src: "images/000002%20(3).jpg", label: "Metal" }, 
+  { src: "images/000002%20(5).jpg", label: "Plastic" }, 
+  { src: "images/000002.jpg", label: "Paper" }, 
+  { src: "images/000003%20(2).jpg", label: "Miscellaneous" }, 
+  { src: "images/000003%20(3).jpg", label: "Plastic" }, 
+  { src: "images/000003%20(6).jpg", label: "Paper" }, 
+  { src: "images/000004%20(4).jpg", label: "Cardboard" }, 
+  { src: "images/000004%20(5).jpg", label: "Metal" }, 
+  { src: "images/000005%20(6).jpg", label: "Plastic" }, 
+  { src: "images/000006%20(7).jpg", label: "Glass" }, 
+  { src: "images/000007%20(4).jpg", label: "Metal" }, 
+  { src: "images/000007%20(7).jpg", label: "Metal" }, 
+  { src: "images/000008%20(4).jpg", label: "Plastic" }, 
+  { src: "images/000008%20(6).jpg", label: "Metal" }, 
+  { src: "images/000011%20(2).jpg", label: "Miscellaneous" }, 
+  { src: "images/000011%20(6).jpg", label: "Metal" }, 
+  { src: "images/000012%20(6).jpg", label: "Glass" }
 ];
 
 let currentImage = null;
