9a9aeaeeea00ed9e69ac24a252d39e456d37aefd
Alex Gan <92696056+NutellaJello@users.noreply.github.com>
2026-04-26 20:37:34 -0400

    Fix image file extensions and correct ID method

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const images = [
-  {src: "images/000000 (2)", label: 'Plastic"},
-  {src: "images/000000 (4)", label: 'Plastic"}, 
-  {src: "images/000000 (6)", label: 'Plastic"},
-  {src: "images/000000 (7)", label: 'Plastic"},
-  {src: "images/000001 (2)", label: 'Paper"},
-  {src: "images/trashed-1746687919-IMG_20250408_123438", label: 'Plastic"},
-  {src: "images/trashed-1746687933-IMG_20250408_123457", label: 'Plastic"}
+  { src: "images/000000 (2).jpg", label: "Plastic" },
+  { src: "images/000000 (4).jpg", label: "Plastic" }, 
+  { src: "images/000000 (6).jpg", label: "Plastic" },
+  { src: "images/000000 (7).jpg", label: "Plastic" },
+  { src: "images/000001 (2).jpg", label: "Paper" },
+  { src: "images/trashed-1746687919-IMG_20250408_123438.jpg", label: "Plastic" },
+  { src: "images/trashed-1746687933-IMG_20250408_123457.jpg", label: "Plastic" }
 ];
 
 let currentImage = null;
@@ -24,7 +24,7 @@ function checkAnswer(userAnswer){
     
   if (userAnswer == currentImage.label){
     score++;
-    document.getElementById("testing").textContent = "Correct!";
+    document.getElementByID("testing").textContent = "Correct!";
   }
   else{
     document.getElementByID("testing").textContent = "Incorrect! Correct answer: " + currentImage.label;
